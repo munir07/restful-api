a3a9d8e59f16c75dc45465f155a13a06da05cfa5
munir07 <munir07@hotmail.com>
2019-08-22 14:57:09 +0800

    workshop03 setup

--- workshop03/main.js
+++ workshop03/main.js
@@ -20,12 +20,12 @@ const keys = require('./keys.json')
 
 console.info(`Using ${keys.mongo}`);
 
-// TODO change your databaseName and collectioName 
+// TODO change your databaseName and collectioName
 // if they are not the defaults below
-const db = CitiesDB({  
-	connectionUrl: keys.mongo, 
-	databaseName: 'zips', 
-	collectionName: 'city'
+const db = CitiesDB({
+	connectionUrl: keys.mongo,
+	databaseName: 'cities',
+	collectionName: 'cities'
 });
 
 const app = express();
@@ -41,6 +41,101 @@ app.use(express.urlencoded({ extended: true }));
 
 // Start of workshop
 // TODO 2/2 Copy your routes from workshop02 here
+
+
+// Mandatory workshop
+// TODO GET /api/states
+app.get('/api/states', (req, resp) => {
+	db.findAllStates()
+		.then(result => {
+			resp.status(200);
+			resp.type('application/json');
+			resp.json(
+				result.map(v => `/api/state/${v}`)
+			);
+		})
+		.catch(error => {
+			resp.status(400);
+			resp.type('text/plain');
+			resp.send(error);
+		})
+})
+
+// TODO HEAD /api/state/:state
+app.head('/api/state/:state', (req, resp) => {
+	const state = req.params.state
+	// check if state exist
+	resp.status(200);
+	resp.type('application/json');
+	resp.header('Accept-Ranges', 'cities');
+	resp.end();
+})
+
+// TODO GET /api/state/:state
+app.get('/api/state/:state', range({accept: 'cities', limit: 20}), (req, resp) => {
+	const state = req.params.state
+	const first = req.range.first;
+	const last = req.range.last;
+	Promise.all([
+		db.findCitiesByState(state, {limit: (last - first + 1), offset: first}),
+		db.countCitiesInState(state)
+	])	.then(results => {
+			resp.status(206);
+			resp.type('application/json');
+			resp.range({
+				first: first,
+				last: last,
+				length: results[1]
+			})
+			resp.json(
+				results[0].map(v => `/api/city/${v}`)
+			);
+		})
+		.catch(error => {
+			resp.status(400);
+			resp.type('text/plain');
+			resp.send(error);
+		})
+})
+
+// TODO GET /api/city/:cityId
+app.get('/api/city/:cityId', (req, resp) => {
+	const cityId = req.params.cityId
+	db.findCityById(cityId)
+		.then(result => {
+			resp.type('application/json');
+			if (result.length == 0) {
+				resp.status(404)
+			    resp.json({message: `City ID ${cityId} not found.`});
+			} else {
+				resp.status(200);
+				resp.json(result[0]);
+			}
+		})
+		.catch(error => {
+			resp.status(400);
+			resp.type('text/plain');
+			resp.send(error);
+		})
+})
+
+// TODO POST /api/city
+app.post('/api/city', (req, resp) => {
+	const data = req.body;
+	db.insertCity(data)
+		.then(result => {
+			resp.status(201);
+			resp.type('application/json');
+			resp.json({ message: 'City added successfully.'});
+		})
+		.catch(error => {
+			resp.status(400);
+			resp.type('text/plain');
+			resp.send(error);
+		})
+})
+
+
 
 
 // End of workshop
